c2d7d9a3d3d91e168b31d52009c16e7c93089719
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-03 16:05:01 -0800

    add private services for auth.client.service.ts

--- src/modules/auth/client/auth.client.service.ts
+++ src/modules/auth/client/auth.client.service.ts
@@ -1,6 +1,87 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { OtpEntity } from '../entities/otp.entity';
+import { Repository } from 'typeorm';
+
+import * as bcrypt from 'bcrypt';
+import { randomInt } from 'crypto';
+import { generateCode } from 'src/common/types/generateCode.type';
 
 @Injectable()
 export class AuthAppService {
-  constructor() {}
+  constructor(
+    @InjectRepository(OtpEntity)
+    private readonly OtpRepository: Repository<OtpEntity>,
+  ) {}
+
+  // private methods
+  private async CheckExpire(otp: OtpEntity) {
+    return otp.expireAt < new Date(Date.now());
+  }
+
+  private async CheckRateForCreateOtp(mobile: string) {
+    const otp = await this.OtpRepository.findOne({
+      where: {
+        mobile,
+      },
+      order: { createdAt: 'DESC' },
+    });
+
+    if (otp) {
+      const RateResult = await this.CheckExpire(otp);
+
+      if (!RateResult) {
+        throw new HttpException(
+          'Too many request for otp',
+          HttpStatus.TOO_MANY_REQUESTS,
+        );
+      }
+    }
+  }
+
+  private async DeleteOtp(otp: OtpEntity) {
+    return await this.OtpRepository.delete({
+      mobile: otp.mobile,
+      code: otp.code,
+    });
+  }
+
+  private async ValidateOtp(mobile: string, otpCode: string) {
+    const otp = await this.OtpRepository.findOne({
+      where: {
+        mobile,
+      },
+      order: { createdAt: 'DESC' },
+    });
+
+    if (!otp) {
+      throw new NotFoundException('There is no otp for this Mobile-number');
+    }
+
+    const isMatch = await bcrypt.compare(otpCode, otp.code);
+
+    if (!isMatch) {
+      throw new BadRequestException('Wrong otp for this mobile-number');
+    }
+
+    return otp;
+  }
+
+  private async generateOtp(): Promise<generateCode> {
+    const otpCode = randomInt(10000, 99999).toString();
+    const hashedOtpCode = await bcrypt.hash(otpCode, 10);
+
+    return {
+      otp: otpCode,
+      hashedOtp: hashedOtpCode,
+    };
+  }
+
+  // public methods
 }
