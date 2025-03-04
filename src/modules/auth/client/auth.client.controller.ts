e9953cbc76cdc200712a9aae3ef58f77eccf8b0e
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-04 13:27:51 -0800

    add SignIn api into app

--- src/modules/auth/client/auth.client.controller.ts
+++ src/modules/auth/client/auth.client.controller.ts
@@ -1,11 +1,24 @@
-import { Body, Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AuthAppService } from './auth.client.service';
 import { SignUpDto } from './dto/signUp.dto';
-import { ApiBody, ApiOperation } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { SignInDto } from './dto/signIn.dto';
+import { JwtService } from '@nestjs/jwt';
 
+@ApiTags('client-Auth')
 @Controller('api/v1/client/auth')
 export class AuthAppController {
-  constructor(private readonly AuthService: AuthAppService) {}
+  constructor(
+    private readonly AuthService: AuthAppService,
+    private readonly JwtService: JwtService,
+  ) {}
 
   //GET -
   @Get('getOtp')
@@ -14,5 +27,17 @@ export class AuthAppController {
   @HttpCode(HttpStatus.OK)
   async getOtp(@Body() data: SignUpDto) {
     return await this.AuthService.createOtp(data.mobile);
+  }
+
+  @Post('SignIn')
+  @ApiBody({ type: SignInDto, description: 'required fields for SignIn' })
+  @ApiOperation({ summary: 'For SignIn intp web app & recive jwt' })
+  @HttpCode(HttpStatus.CREATED)
+  async SignIn(@Body() SignInDto: SignInDto) {
+    await this.AuthService.VerifyOtp(SignInDto);
+
+    const userToken = await this.AuthService.getUser(SignInDto);
+
+    return { access_token: userToken };
   }
 }
