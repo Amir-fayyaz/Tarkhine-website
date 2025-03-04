0dc336b1316ddb98adc5e96ec1385837418def44
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-04 13:25:06 -0800

    add SignIn services in auth.client.service.ts

--- src/modules/auth/client/auth.client.service.ts
+++ src/modules/auth/client/auth.client.service.ts
@@ -12,12 +12,20 @@ import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { randomInt } from 'crypto';
 import { generateCode } from 'src/common/types/generateCode.type';
+import { SignInDto } from './dto/signIn.dto';
+import { AuthAppFactory } from './auth.client.factory';
+import { JwtService } from '@nestjs/jwt';
+import { config } from 'dotenv';
+
+config();
 
 @Injectable()
 export class AuthAppService {
   constructor(
     @InjectRepository(OtpEntity)
     private readonly OtpRepository: Repository<OtpEntity>,
+    private readonly AuthAppFactory: AuthAppFactory,
+    private readonly JwtService: JwtService,
   ) {}
 
   // private methods
@@ -98,5 +106,47 @@ export class AuthAppService {
     await this.OtpRepository.save(newOtp);
 
     return newOtpCode.otp;
+  }
+
+  public async VerifyOtp(data: SignInDto) {
+    const otp = await this.ValidateOtp(data.mobile, data.otpCode);
+
+    const exprireResult = await this.CheckExpire(otp);
+
+    if (exprireResult) {
+      throw new BadRequestException('Otp has expired');
+    }
+
+    await this.DeleteOtp(otp);
+  }
+
+  public async getUser(data: SignInDto) {
+    const user = await this.AuthAppFactory.FindUserByMobile(data.mobile);
+
+    if (user) {
+      return await this.JwtService.signAsync(
+        {
+          userId: user.id,
+          mobile: user.mobile,
+        },
+        {
+          secret: process.env.JWT_SECRET,
+        },
+      );
+    } else {
+      const newUser = await this.AuthAppFactory.CreateUser({
+        mobile: data.mobile,
+      });
+
+      return await this.JwtService.signAsync(
+        {
+          userId: newUser.id,
+          mobile: newUser.mobile,
+        },
+        {
+          secret: process.env.JWT_SECRET,
+        },
+      );
+    }
   }
 }
