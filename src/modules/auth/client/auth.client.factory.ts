8a8442ff6e546cb8e8341755b8f4477a6d4a4293
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-04 09:01:47 -0800

    setup jwt-module for auth

--- src/modules/auth/client/auth.client.factory.ts
+++ src/modules/auth/client/auth.client.factory.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { UserAppService } from 'src/modules/users/client/user.client.service';
+import { ICreateUser } from '../interfaces/ICreateUser.interface';
+import { FindUserByMobile } from '../interfaces/IFindUserByMobile.interface';
+import { CreateUserDto } from 'src/modules/users/client/dto/create-user.dto';
 
 @Injectable()
-export class AuthAppFactory {}
+export class AuthAppFactory {
+  private readonly Create: ICreateUser;
+  private readonly FindUserByMobilePhone: FindUserByMobile;
+
+  constructor(UserService: UserAppService) {
+    this.Create = UserService;
+    this.FindUserByMobilePhone = UserService;
+  }
+
+  public async CreateUser(data: CreateUserDto) {
+    return await this.Create.CreateUser(data);
+  }
+
+  public async FindUserByMobile(mobile: string) {
+    return await this.FindUserByMobilePhone.FindUserByMobile(mobile);
+  }
+}
--- src/modules/auth/client/auth.client.module.ts
+++ src/modules/auth/client/auth.client.module.ts
@@ -6,10 +6,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OtpEntity } from '../entities/otp.entity';
 import { UserAppService } from 'src/modules/users/client/user.client.service';
 import { UserEntity } from 'src/modules/users/entities/user.entity';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
+import { config } from 'dotenv';
+config();
+
+const { JWT_SECRET, JWT_DEV_EXPIRE } = process.env;
 @Module({
-  imports: [TypeOrmModule.forFeature([OtpEntity, UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([OtpEntity, UserEntity]),
+    JwtModule.register({
+      secret: JWT_SECRET,
+      signOptions: {
+        expiresIn: JWT_DEV_EXPIRE,
+      },
+    }),
+  ],
   controllers: [AuthAppController],
-  providers: [AuthAppService, AuthAppFactory, UserAppService],
+  providers: [AuthAppService, AuthAppFactory, UserAppService, JwtService],
 })
 export class AuthAppModule {}
