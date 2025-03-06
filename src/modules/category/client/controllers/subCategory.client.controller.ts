aed63053b9ffd9a1b02fb66af971e0436409cf18
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-06 14:06:11 -0800

    add getSubCategories api to app

--- src/modules/category/client/controllers/subCategory.client.controller.ts
+++ src/modules/category/client/controllers/subCategory.client.controller.ts
@@ -1,8 +1,38 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  HttpCode,
+  HttpStatus,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import { SubCategoryAppService } from '../services/subCategory.client.service';
 
 @Controller('api/v1/client/subcategory')
 @ApiTags('client-SubCategory')
+@ApiBearerAuth()
 export class SubCategoryAppController {
-  constructor() {}
+  constructor(private readonly SubCategoryService: SubCategoryAppService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'For recive subCategories with pagination' })
+  @ApiQuery({
+    name: 'page',
+    default: 1,
+    required: false,
+    description: 'For pagination',
+  })
+  @HttpCode(HttpStatus.OK)
+  async getSubCategories(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    return await this.SubCategoryService.getSubCategories(page);
+  }
 }
