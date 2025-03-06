f0d489f03e644d9b89ce25d32ff85e3700474b64
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-06 14:35:04 -0800

    add getCategories api into category.admin.controller.ts

--- src/modules/category/admin/controllers/category.admin.controller.ts
+++ src/modules/category/admin/controllers/category.admin.controller.ts
@@ -1,6 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  HttpCode,
+  HttpStatus,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
+import { CategoryAdminService } from '../services/category.admin.service';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('api/v1/admin/category')
+@ApiTags('admin-category')
+@ApiBearerAuth()
 export class CategoryAdminController {
-  constructor() {}
+  constructor(private readonly CategoryService: CategoryAdminService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'For recive categories with pagination' })
+  @ApiQuery({
+    name: 'page',
+    default: 1,
+    required: false,
+    description: 'For pagination',
+  })
+  @HttpCode(HttpStatus.OK)
+  async getCategories(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    return await this.CategoryService.getCategories(page);
+  }
 }
