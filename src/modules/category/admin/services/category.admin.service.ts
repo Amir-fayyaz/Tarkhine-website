54591158d66b7ee14ffd06177890e3090a92520e
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-06 14:34:36 -0800

    add getCategories service into category.admin.service.ts

--- src/modules/category/admin/services/category.admin.service.ts
+++ src/modules/category/admin/services/category.admin.service.ts
@@ -1,6 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryEntity } from '../../entities/category.entity';
+import { Repository } from 'typeorm';
+import { Pagination } from 'src/common/tools/pagination.tool';
 
 @Injectable()
 export class CategoryAdminService {
-  constructor() {}
+  constructor(
+    @InjectRepository(CategoryEntity)
+    private readonly Category_Repository: Repository<CategoryEntity>,
+  ) {}
+
+  //private methods
+
+  //public methods
+  public async getCategories(page: number) {
+    const pagination = Pagination({ page, take: 20 });
+
+    const categories = await this.Category_Repository.find({
+      order: { createdAt: 'DESC' },
+      take: pagination.take,
+      skip: pagination.skip,
+    });
+
+    if (categories.length < 1) {
+      throw new NotFoundException('There is no category');
+    }
+
+    return {
+      page,
+      categories,
+      count: categories.length,
+    };
+  }
 }
