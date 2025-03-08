3111a16c4673870e5d60b5b95e07f48b29de3184
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-08 02:53:23 -0800

    add create-SubCategory api into subCategory.admin.controller.ts

--- src/modules/category/admin/controllers/subCategory.admin.controller.ts
+++ src/modules/category/admin/controllers/subCategory.admin.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { SubCategoryAdminService } from '../services/subCategory.admin.service';
+import { CreateSubCategoryDto } from '../dto/create-SubCategory.dto';
 
 @Controller('api/v1/admin/subcategory')
 export class SubCategoryAdminController {
-  constructor() {}
+  constructor(private readonly SubCategoryService: SubCategoryAdminService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async createSubCategory(@Body() data: CreateSubCategoryDto) {
+    return await this.SubCategoryService.createSubCategory(data);
+  }
 }
