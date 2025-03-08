fb1309c3ce20a7190401244c1879098b7376a1bd
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-08 03:22:36 -0800

    add getSubCategory for special category api into subCategory.admin.controller.ts

--- src/modules/category/admin/controllers/subCategory.admin.controller.ts
+++ src/modules/category/admin/controllers/subCategory.admin.controller.ts
@@ -1,14 +1,50 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { SubCategoryAdminService } from '../services/subCategory.admin.service';
 import { CreateSubCategoryDto } from '../dto/create-SubCategory.dto';
+import { ApiBody, ApiOperation, ApiParam, ApiQuery } from '@nestjs/swagger';
 
 @Controller('api/v1/admin/subcategory')
 export class SubCategoryAdminController {
   constructor(private readonly SubCategoryService: SubCategoryAdminService) {}
 
   @Post()
+  @ApiOperation({ summary: 'For create new subCategory' })
+  @ApiBody({ type: CreateSubCategoryDto, description: 'required fields' })
   @HttpCode(HttpStatus.CREATED)
   async createSubCategory(@Body() data: CreateSubCategoryDto) {
     return await this.SubCategoryService.createSubCategory(data);
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'For recive subCategory of special category with pagination',
+  })
+  @ApiParam({ name: 'id', description: 'category_id' })
+  @ApiQuery({
+    name: 'page',
+    description: 'For pagination',
+    default: 1,
+    required: false,
+  })
+  @HttpCode(HttpStatus.OK)
+  async getSubCategoriesForCategory(
+    @Param('id', ParseIntPipe) category_id: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    return await this.SubCategoryService.getSubCategoriesForCategory(
+      category_id,
+      page,
+    );
   }
 }
