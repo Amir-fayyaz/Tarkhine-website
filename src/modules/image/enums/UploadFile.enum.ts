f972e2f203b3e50c53df915e4062361e36a890f5
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-08 22:41:05 -0800

    add uploadFile api into image.controller.ts

---
+++ src/modules/image/enums/UploadFile.enum.ts
@@ -0,0 +1,4 @@
+export enum UploadFileType {
+  PROFILE = 'profile',
+  FOOD = 'food',
+}
--- src/modules/image/image.controller.ts
+++ src/modules/image/image.controller.ts
@@ -1,7 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ImageService } from './image.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { MulterOption } from 'src/common/configs/Multer.config';
+import { ApiBody, ApiConsumes, ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { Request } from 'express';
+import { UploadFileType } from './enums/UploadFile.enum';
 
 @Controller('api/v1/image')
 export class ImageController {
   constructor(private readonly ImageService: ImageService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'For upload file into storage' })
+  @ApiQuery({
+    name: 'uploadType',
+    description: 'For undrastand of where should i save this file',
+    enum: UploadFileType,
+  })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'For upload file',
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @HttpCode(HttpStatus.OK)
+  @UseInterceptors(FileInterceptor('file', MulterOption))
+  async UploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Req() request: Request,
+  ) {
+    if (!request.file) throw new BadRequestException('Send file please');
+    return await this.ImageService.uploadFile(file);
+  }
 }
