d0fd10795eaa42c8a6454aa6d2aebb0d99ec0f4f
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-10 10:42:18 -0700

    add product entity into app

---
+++ src/modules/products/entities/product.entity.ts
@@ -0,0 +1,26 @@
+import { Base } from 'src/common/abstracts/base.entity';
+import { CategoryEntity } from 'src/modules/category/entities/category.entity';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+
+@Entity('product')
+export class ProductEntity extends Base {
+  @Column({ type: 'varchar', nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  description: string;
+
+  @Column({ type: 'numeric', nullable: false })
+  price: number;
+
+  @Column({ type: 'varchar', nullable: true })
+  image_url: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  star: string;
+
+  //relations
+  @ManyToOne(() => CategoryEntity, (category) => category.products)
+  @JoinColumn({ name: 'category' })
+  category: CategoryEntity[];
+}
