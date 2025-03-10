a0dff9cbaf96b7828c24b4e3aaecd120e0aca748
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-10 10:42:40 -0700

    add relations for product.entity.ts

--- src/modules/category/entities/category.entity.ts
+++ src/modules/category/entities/category.entity.ts
@@ -1,12 +1,17 @@
 import { Base } from 'src/common/abstracts/base.entity';
 import { Column, Entity, OneToMany } from 'typeorm';
 import { SubCategoryEntity } from './subCategory.entity';
+import { ProductEntity } from 'src/modules/products/entities/product.entity';
 
 @Entity('category')
 export class CategoryEntity extends Base {
   @Column({ type: 'varchar', nullable: false, unique: true })
   title: string;
 
+  //relations
   @OneToMany(() => SubCategoryEntity, (subCategory) => subCategory.category)
   subcategories: SubCategoryEntity[];
+
+  @OneToMany(() => ProductEntity, (product) => product.category)
+  products: ProductEntity[];
 }
