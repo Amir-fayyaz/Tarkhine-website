688948e5975c1d31f87e2d7d21d35a907149d184
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-10 12:01:04 -0700

    add requirments into product.admin.module.ts

--- src/modules/products/admin/product.admin.module.ts
+++ src/modules/products/admin/product.admin.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ProductAdminController } from './controllers/product.admin.controller';
+import { ProductAdminService } from './services/product.admin.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from '../entities/product.entity';
+import { JwtService } from '@nestjs/jwt';
+import { AuthAdminFactory } from 'src/modules/auth/admin/auth.admin.factory';
+import { AuthAdminService } from 'src/modules/auth/admin/auth.admin.service';
+import { AdminEntity } from 'src/modules/auth/entities/admin.entity';
+import { ProductAdminFactory } from './product.admin.factory';
+import { CategoryAdminService } from 'src/modules/category/admin/services/category.admin.service';
+import { CategoryEntity } from 'src/modules/category/entities/category.entity';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forFeature([ProductEntity, AdminEntity, CategoryEntity]),
+  ],
+  controllers: [ProductAdminController],
+  providers: [
+    ProductAdminService,
+    JwtService,
+    AuthAdminFactory,
+    AuthAdminService,
+    ProductAdminFactory,
+    CategoryAdminService,
+  ],
 })
 export class ProductAdminModule {}
