8f4e9a541df5f4525040dc5697fb6c9592243a38
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-12 09:56:02 -0700

    add requirments into category.client.module.ts

--- src/modules/category/client/category.client.module.ts
+++ src/modules/category/client/category.client.module.ts
@@ -7,10 +7,21 @@ import { AuthAppFactory } from 'src/modules/auth/client/auth.client.factory';
 import { UserAppService } from 'src/modules/users/client/user.client.service';
 import { UserEntity } from 'src/modules/users/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
+import { SubCategoryAppController } from './controllers/subCategory.client.controller';
+import { SubCategoryAppService } from './services/subCategory.client.service';
+import { SubCategoryEntity } from '../entities/subCategory.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CategoryEntity, UserEntity])],
-  controllers: [CategoryAppController],
-  providers: [CategoryAppService, AuthAppFactory, UserAppService, JwtService],
+  imports: [
+    TypeOrmModule.forFeature([CategoryEntity, UserEntity, SubCategoryEntity]),
+  ],
+  controllers: [CategoryAppController, SubCategoryAppController],
+  providers: [
+    CategoryAppService,
+    AuthAppFactory,
+    UserAppService,
+    JwtService,
+    SubCategoryAppService,
+  ],
 })
 export class CategoryAppModule {}
