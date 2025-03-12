a755493161b38918ad5d1dd324cf011cd67d7a43
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-12 14:06:08 -0700

    add getProducts BySubCategory api into product.client.controller.ts

---
+++ src/modules/products/client/controllers/product.client.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UserGuard } from 'src/modules/auth/guards/User.guard';
+import { ProductAppService } from '../services/product.client.service';
+
+@Controller('api/v1/client/products')
+@ApiTags('client-product')
+@ApiBearerAuth()
+@UseGuards(UserGuard)
+export class ProductAppController {
+  constructor(private readonly ProductService: ProductAppService) {}
+
+  //GET -
+  @Get(':id')
+  @ApiOperation({ summary: 'For get products with special subCategory' })
+  @ApiParam({ name: 'id', description: 'subCategory-id' })
+  @ApiQuery({
+    name: 'page',
+    description: 'For pagination',
+    required: false,
+    default: 1,
+  })
+  @HttpCode(HttpStatus.OK)
+  async getProducts(
+    @Param('id', ParseIntPipe) subCategory_id: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    return await this.ProductService.getProductsBySubCategory(
+      page,
+      subCategory_id,
+    );
+  }
+}
