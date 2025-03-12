79108da683039a80be131c643688b6e3992c0f02
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-12 15:05:06 -0700

    add likeOrDisLike api into like.client.controller.ts

---
+++ src/modules/products/client/controllers/like.client.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UserGuard } from 'src/modules/auth/guards/User.guard';
+import { LikeAppService } from '../services/like.client.service';
+import { User } from 'src/common/decorators/getUser.decorator';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
+
+@Controller('api/v1/client/likes')
+@ApiTags('client-like')
+@ApiBearerAuth()
+@UseGuards(UserGuard)
+export class LikeAppController {
+  constructor(private readonly LikeService: LikeAppService) {}
+
+  @Post(':id')
+  @ApiOperation({ summary: 'For like or dislike product' })
+  @ApiParam({ name: 'id', description: 'product-id' })
+  @HttpCode(HttpStatus.OK)
+  async LikeOrDisLikeProduct(
+    @User() user: UserEntity,
+    @Param('id', ParseIntPipe) product_id: number,
+  ) {
+    return await this.LikeService.likeOrDisLikeProduct(product_id, user);
+  }
+}
