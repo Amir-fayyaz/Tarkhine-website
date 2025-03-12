52732c02c0e5f0fd3776e7b7bb3c8d3f5a80211f
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-12 08:45:48 -0700

    add FindCategoryById api into subCategory.admin.controller.ts

--- src/modules/category/admin/controllers/subCategory.admin.controller.ts
+++ src/modules/category/admin/controllers/subCategory.admin.controller.ts
@@ -1,14 +1,23 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
+  Param,
+  ParseIntPipe,
   Post,
   UseGuards,
 } from '@nestjs/common';
 import { SubCategoryAdminService } from '../services/subCategory.admin.service';
 import { CreateSubCategoryDto } from '../dto/create-SubCategory.dto';
-import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AdminGuard } from 'src/modules/auth/guards/Admin.guard';
 
 @ApiTags('admin-subCategory')
@@ -18,11 +27,21 @@ import { AdminGuard } from 'src/modules/auth/guards/Admin.guard';
 export class SubCategoryAdminController {
   constructor(private readonly SubCategoryService: SubCategoryAdminService) {}
 
+  //POST -
   @Post()
   @ApiOperation({ summary: 'For create new subCategory' })
   @ApiBody({ type: CreateSubCategoryDto, description: 'required fields' })
   @HttpCode(HttpStatus.CREATED)
   async CreateSubCategory(@Body() data: CreateSubCategoryDto) {
     return await this.SubCategoryService.createSubCategory(data);
+  }
+
+  //GET -
+  @Get(':id')
+  @ApiOperation({ summary: 'For Find subCategory by id' })
+  @ApiParam({ name: 'id', description: 'subCategory-id' })
+  @HttpCode(HttpStatus.OK)
+  async getSubCategoryById(@Param('id', ParseIntPipe) subCategory_id: number) {
+    return await this.SubCategoryService.getSubCategoryById(subCategory_id);
   }
 }
