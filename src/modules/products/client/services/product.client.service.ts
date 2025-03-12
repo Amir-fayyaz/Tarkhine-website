a4fb82786cc13b4f2fb15ce490ed99fb0b9888d3
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-12 14:03:44 -0700

    add getProducts BySubCategory service into product.client.service.ts

---
+++ src/modules/products/client/services/product.client.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductEntity } from '../../entities/product.entity';
+import { Repository } from 'typeorm';
+import { Pagination } from 'src/common/tools/pagination.tool';
+
+@Injectable()
+export class ProductAppService {
+  constructor(
+    @InjectRepository(ProductEntity)
+    private readonly Product_repository: Repository<ProductEntity>,
+  ) {}
+
+  //private methods
+
+  //public methods
+  public async getProductsBySubCategory(page: number, subCategory_id: number) {
+    const pagination = Pagination({ page, take: 20 });
+
+    const products = await this.Product_repository.find({
+      order: { createdAt: 'DESC' },
+      relations: ['category', 'subCategory'],
+      skip: pagination.skip,
+      take: pagination.take,
+    });
+
+    return {
+      page,
+      products,
+      count: products.length,
+    };
+  }
+}
