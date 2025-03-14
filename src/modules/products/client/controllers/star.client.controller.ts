a492cf71963725c6d1b3fd116ce6747b122cd04a
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-14 12:28:50 -0700

    add delete-StarRate api into star.client.controller.ts

--- src/modules/products/client/controllers/star.client.controller.ts
+++ src/modules/products/client/controllers/star.client.controller.ts
@@ -1,14 +1,23 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
+  Param,
+  ParseIntPipe,
   Post,
   UseGuards,
 } from '@nestjs/common';
 import { StarAppService } from '../services/star.client.service';
-import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserGuard } from 'src/modules/auth/guards/User.guard';
 import { CreateStarDto } from '../dto/create-star.dto';
 import { User } from 'src/common/decorators/getUser.decorator';
@@ -27,13 +36,24 @@ export class StarAppController {
   @ApiBody({ type: CreateStarDto, description: 'required fields' })
   @HttpCode(HttpStatus.OK)
   async createStar(@Body() data: CreateStarDto, @User() user: UserEntity) {
-    return await this.starService.createOrUpdateStar(data, user);
+    return await this.starService.createOrUpdateStarRate(data, user);
   }
 
   @Get()
   @ApiOperation({ summary: 'For fetch all stars that user  rated on products' })
   @HttpCode(HttpStatus.OK)
   async getUserStars(@User() user: UserEntity) {
-    return await this.starService.getUserStars(user);
+    return await this.starService.getUserStarsRate(user);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'For delete starRate from product' })
+  @ApiParam({ name: 'id', description: 'product-id' })
+  @HttpCode(HttpStatus.OK)
+  async deleteStarRate(
+    @User() user: UserEntity,
+    @Param('id', ParseIntPipe) product_id: number,
+  ) {
+    return await this.starService.deleteStarRate(product_id, user.id);
   }
 }
