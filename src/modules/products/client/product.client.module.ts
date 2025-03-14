e5baab4903e10f8f2e233820aa9819c561f88dc4
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-14 09:46:09 -0700

    add requirment into product.client.module.ts

--- src/modules/products/client/product.client.module.ts
+++ src/modules/products/client/product.client.module.ts
@@ -1,31 +1,48 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { LikeEntity } from '../entities/like.entity';
-import { ProductAdminService } from '../admin/services/product.admin.service';
 import { ProductEntity } from '../entities/product.entity';
-import { ProductAdminFactory } from '../admin/product.admin.factory';
-import { ImageService } from 'src/modules/image/image.service';
+import { ProductAppController } from './controllers/product.client.controller';
+import { AuthAppFactory } from 'src/modules/auth/client/auth.client.factory';
+import { UserAppService } from 'src/modules/users/client/user.client.service';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { ProductAppService } from './services/product.client.service';
+import { LikeEntity } from '../entities/like.entity';
+import { LikeAppController } from './controllers/like.client.controller';
+import { LikeAppService } from './services/like.client.service';
+import { ProductAdminService } from '../admin/services/product.admin.service';
 import { CategoryAdminService } from 'src/modules/category/admin/services/category.admin.service';
-import { CategoryEntity } from 'src/modules/category/entities/category.entity';
 import { SubCategoryAdminService } from 'src/modules/category/admin/services/subCategory.admin.service';
+import { CategoryEntity } from 'src/modules/category/entities/category.entity';
 import { SubCategoryEntity } from 'src/modules/category/entities/subCategory.entity';
+import { ImageService } from 'src/modules/image/image.service';
+import { StarAppService } from './services/star.client.service';
+import { StarAppController } from './controllers/star.client.controller';
+import { StarEntity } from '../entities/stars.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
-      LikeEntity,
       ProductEntity,
+      UserEntity,
+      LikeEntity,
       CategoryEntity,
       SubCategoryEntity,
+      StarEntity,
     ]),
   ],
-  controllers: [],
+  controllers: [ProductAppController, LikeAppController, StarAppController],
   providers: [
+    AuthAppFactory,
+    UserAppService,
+    JwtService,
+    ProductAppService,
+    LikeAppService,
     ProductAdminService,
-    ProductAdminFactory,
-    ImageService,
     CategoryAdminService,
     SubCategoryAdminService,
+    ImageService,
+    StarAppService,
   ],
 })
 export class ProductAppModule {}
