0207922cc41f5f1d5c5bdce534ccc8b02922b85a
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-17 05:47:44 -0700

    add category filter into getSubCategories api into subCategory.client.controller.ts

--- src/modules/category/client/controllers/subCategory.client.controller.ts
+++ src/modules/category/client/controllers/subCategory.client.controller.ts
@@ -4,6 +4,7 @@ import {
   Get,
   HttpCode,
   HttpStatus,
+  Param,
   ParseIntPipe,
   Query,
   UseGuards,
@@ -12,6 +13,7 @@ import { SubCategoryAppService } from '../services/subCategory.client.service';
 import {
   ApiBearerAuth,
   ApiOperation,
+  ApiParam,
   ApiQuery,
   ApiTags,
 } from '@nestjs/swagger';
@@ -24,18 +26,11 @@ import { UserGuard } from 'src/modules/auth/guards/User.guard';
 export class SubCategoryAppController {
   constructor(private readonly SubCategoryService: SubCategoryAppService) {}
 
-  @Get()
+  @Get('category/:id')
   @ApiOperation({ summary: 'get subCategories with pagination' })
-  @ApiQuery({
-    name: 'page',
-    required: false,
-    default: 1,
-    description: 'For pagination',
-  })
+  @ApiParam({ name: 'id', description: 'category-id' })
   @HttpCode(HttpStatus.OK)
-  async getSubCategories(
-    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
-  ) {
-    return await this.SubCategoryService.getSubCategories(page);
+  async getSubCategories(@Param('id', ParseIntPipe) category_id: number) {
+    return await this.SubCategoryService.getSubCategories(category_id);
   }
 }
