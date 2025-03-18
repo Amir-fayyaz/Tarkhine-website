eb86d117c75a4a9358c9798b17b922cf80fe9b8d
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-18 11:09:39 -0700

    add pagination for getProducts

--- src/modules/products/client/services/product.client.service.ts
+++ src/modules/products/client/services/product.client.service.ts
@@ -14,14 +14,19 @@ export class ProductAppService {
   //private methods
 
   //public methods
-  public async getProductsBySubCategory(subCategory_id: number) {
+  public async getProductsBySubCategory(page: number, subCategory_id: number) {
+    const pagination = Pagination({ page, take: 20 });
+
     const products = await this.Product_repository.find({
       where: { subCategory: { id: subCategory_id } },
       order: { createdAt: 'DESC' },
       relations: ['category', 'subCategory', 'coupon'],
+      skip: pagination.skip,
+      take: pagination.take,
     });
 
     return {
+      page,
       products,
       count: products.length,
     };
