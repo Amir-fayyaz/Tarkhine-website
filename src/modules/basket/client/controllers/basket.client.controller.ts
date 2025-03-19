4cb870958b8558451bf2976e50f5f7a764701677
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-19 09:09:29 -0700

    add updateProductQuantityInBasket api into basket.client.controller.ts

--- src/modules/basket/client/controllers/basket.client.controller.ts
+++ src/modules/basket/client/controllers/basket.client.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  Put,
   UseGuards,
 } from '@nestjs/common';
 import {
@@ -20,6 +21,7 @@ import { BasketAppService } from '../services/basket.client.service';
 import { AddProductToBasketDto } from '../dto/addProductToBasket.dto';
 import { User } from 'src/common/decorators/getUser.decorator';
 import { UserEntity } from 'src/modules/users/entities/user.entity';
+import { UpdateProductQuantityDto } from '../dto/updateQuantity.dto';
 
 @Controller('api/v1/client/baskets')
 @ApiTags('client-basket')
@@ -28,6 +30,7 @@ import { UserEntity } from 'src/modules/users/entities/user.entity';
 export class BasketAppController {
   constructor(private readonly BasketService: BasketAppService) {}
 
+  //POST -
   @Post(':id')
   @ApiOperation({ summary: 'For add product to user-basket' })
   @ApiBody({
@@ -42,5 +45,17 @@ export class BasketAppController {
     @User() user: UserEntity,
   ) {
     return await this.BasketService.AddProductToBasket(data, product_id, user);
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: 'For upadate product-quantity in basket' })
+  @ApiBody({ type: UpdateProductQuantityDto, description: 'requirment fields' })
+  @ApiParam({ name: 'id', description: 'product-id' })
+  async UpdateProductQuantityInBasket(
+    @Param('id', ParseIntPipe) product_id: number,
+    @Body() data: UpdateProductQuantityDto,
+    @User() user: UserEntity,
+  ) {
+    return await this.BasketService.UpdateQuantity(data, product_id, user);
   }
 }
