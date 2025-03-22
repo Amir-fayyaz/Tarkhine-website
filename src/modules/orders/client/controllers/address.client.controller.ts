3e2f12070071630ce0e0cfaeaff3887d519c77e6
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-22 03:54:24 -0700

    add AddNewAddress api into address.client.controller.ts

---
+++ src/modules/orders/client/controllers/address.client.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AddressAppService } from '../services/address.client.service';
+import { CreateAddressDTO } from '../dto/address/create-address.dto';
+import { User } from 'src/common/decorators/getUser.decorator';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { UserGuard } from 'src/modules/auth/guards/User.guard';
+
+@Controller('api/v1/client/address')
+@ApiTags('client-address')
+@ApiBearerAuth()
+@UseGuards(UserGuard)
+export class AddressAppController {
+  constructor(private readonly AddressService: AddressAppService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'For create new address' })
+  @ApiBody({ type: CreateAddressDTO, description: 'required fields' })
+  async AddNewAddress(
+    @Body() data: CreateAddressDTO,
+    @User() user: UserEntity,
+  ) {
+    return await this.AddressService.AddNewAddress(data, user);
+  }
+}
