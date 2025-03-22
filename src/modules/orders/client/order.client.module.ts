c5de1f4595bea32c92ab4cf927bea57500b8617b
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-22 03:43:49 -0700

    add userGuard requiremnts to order.client.module.ts

--- src/modules/orders/client/order.client.module.ts
+++ src/modules/orders/client/order.client.module.ts
@@ -3,10 +3,23 @@ import { OrderAppController } from './controllers/order.client.controller';
 import { OrderAppService } from './services/order.client.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderEntity } from '../entities/order.entity';
+import { AddressAppController } from './controllers/address.client.controller';
+import { AddressAppService } from './services/address.client.service';
+import { AddressEntity } from '../entities/address.entity';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
+import { UserAppService } from 'src/modules/users/client/user.client.service';
+import { AuthAppFactory } from 'src/modules/auth/client/auth.client.factory';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderEntity])],
-  controllers: [OrderAppController],
-  providers: [OrderAppService],
+  imports: [TypeOrmModule.forFeature([OrderEntity, AddressEntity, UserEntity])],
+  controllers: [OrderAppController, AddressAppController],
+  providers: [
+    OrderAppService,
+    AddressAppService,
+    UserAppService,
+    AuthAppFactory,
+    JwtService,
+  ],
 })
 export class OrderAppModule {}
