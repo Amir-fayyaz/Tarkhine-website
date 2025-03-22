af5dfffeaf861f33a2f5964c0d185777484804c2
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-22 04:44:18 -0700

    add deleteUserAddress api in address.client.controller.ts

--- src/modules/orders/client/controllers/address.client.controller.ts
+++ src/modules/orders/client/controllers/address.client.controller.ts
@@ -1,17 +1,27 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
+  Param,
+  ParseIntPipe,
   Post,
+  Put,
   UseGuards,
 } from '@nestjs/common';
 import { AddressAppService } from '../services/address.client.service';
 import { CreateAddressDTO } from '../dto/address/create-address.dto';
 import { User } from 'src/common/decorators/getUser.decorator';
 import { UserEntity } from 'src/modules/users/entities/user.entity';
-import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserGuard } from 'src/modules/auth/guards/User.guard';
 
 @Controller('api/v1/client/address')
@@ -33,8 +43,19 @@ export class AddressAppController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'For recive all user addresses' })
   @HttpCode(HttpStatus.OK)
   async getUserAddress(@User() user: UserEntity) {
     return await this.AddressService.getUserAddress(user);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'For delete address' })
+  @ApiParam({ name: 'id', description: 'address-id' })
+  async deleteUserAddress(
+    @Param('id', ParseIntPipe) address_id: number,
+    @User() user: UserEntity,
+  ) {
+    return await this.AddressService.deleteUserAddress(address_id, user.id);
   }
 }
