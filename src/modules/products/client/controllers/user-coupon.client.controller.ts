80bce74316f970bf57022998d47a36739422f245
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-23 11:13:55 -0700

    add use-userCoupon api into user-coupon.client.controller.ts

--- src/modules/products/client/controllers/user-coupon.client.controller.ts
+++ src/modules/products/client/controllers/user-coupon.client.controller.ts
@@ -13,6 +13,7 @@ import { UserGuard } from 'src/modules/auth/guards/User.guard';
 import { User } from 'src/common/decorators/getUser.decorator';
 import { UserEntity } from 'src/modules/users/entities/user.entity';
 import { ValidateCouponDTO } from '../dto/validate-userCoupon.dto';
+import { UseUserCouponDto } from '../dto/use-userCoupon.dto';
 
 @Controller('api/v1/client/userCoupon')
 @ApiTags('client-userCoupon')
@@ -39,5 +40,15 @@ export class UserCouponAppController {
       data.coupon_code,
       user.id,
     );
+  }
+
+  @Post('use')
+  @ApiOperation({ summary: 'For using coupon by user' })
+  @ApiBody({ type: UseUserCouponDto })
+  async useUserCoupon(
+    @Body() data: UseUserCouponDto,
+    @User() user: UserEntity,
+  ) {
+    return await this.UserCouponService.useUserCouponForBasket(data, user.id);
   }
 }
