49da4689aa0a6393edbf6693c12456cc96415db0
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-24 14:55:36 -0700

    add requirments

--- src/modules/payment/client/payment.client.module.ts
+++ src/modules/payment/client/payment.client.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { PaymnetAppController } from './payment.client.controller';
+import { PaymentAppService } from './payment.client.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
+import { UserAppService } from 'src/modules/users/client/user.client.service';
+import { PaymentEntity } from '../entities/payment.entity';
+import { ZarinPalService } from 'src/common/services/payment/zarinPal-service.service';
+import { AuthAppFactory } from 'src/modules/auth/client/auth.client.factory';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([UserEntity, PaymentEntity])],
+  controllers: [PaymnetAppController],
+  providers: [
+    PaymentAppService,
+    ZarinPalService,
+    AuthAppFactory,
+    UserAppService,
+    JwtService,
+  ],
 })
 export class PaymentAppModule {}
--- src/modules/payment/payment.module.ts
+++ src/modules/payment/payment.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PaymentAppModule } from './client/payment.client.module';
+import { PaymentAdminModule } from './admin/payment.admin.module';
 
 @Module({
-  imports: [],
+  imports: [PaymentAppModule, PaymentAdminModule],
   controllers: [],
   providers: [],
 })
