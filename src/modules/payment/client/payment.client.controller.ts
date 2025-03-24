b40c26cd4f2fd19d84231b3253c5dd591e716ee1
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-24 16:50:27 -0700

    add verify-payment api into payment.client.controller.ts

--- src/modules/payment/client/payment.client.controller.ts
+++ src/modules/payment/client/payment.client.controller.ts
@@ -4,14 +4,22 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 import { PaymentAppService } from './payment.client.service';
 import { UserGuard } from 'src/modules/auth/guards/User.guard';
 import { User } from 'src/common/decorators/getUser.decorator';
 import { UserEntity } from 'src/modules/users/entities/user.entity';
-import { RequestPayment } from './dto/requestPayment.dto';
+import { RequestPaymentDto } from './dto/requestPayment.dto';
+import { VerfiyPaymentDto } from './dto/verifyPayment.dto';
 
 @Controller('api/v1/client/payment')
 @ApiTags('client-payment')
@@ -22,9 +30,40 @@ export class PaymnetAppController {
 
   @Post('request')
   @ApiOperation({ summary: 'For create request-payment for user' })
-  @ApiBody({ type: RequestPayment })
+  @ApiBody({ type: RequestPaymentDto })
   @HttpCode(HttpStatus.OK)
-  async requestPayment(@Body() data: RequestPayment, @User() user: UserEntity) {
+  async requestPayment(
+    @Body() data: RequestPaymentDto,
+    @User() user: UserEntity,
+  ) {
     return await this.PaymentService.requestPayment(data, user);
+  }
+
+  @Post('verify')
+  @ApiOperation({ description: 'For verify user payment' })
+  @ApiQuery({
+    name: 'Authority',
+    type: String,
+    example: 'S000000000000000000000000000000qymdq',
+  })
+  @ApiQuery({ name: 'Status', type: String, example: 'OK' })
+  async verifyPayment(
+    @Query('Authority') authority: string,
+    @Query('Status') status: string,
+    @Body() data: VerfiyPaymentDto,
+  ) {
+    console.log(authority, status, data.amount);
+    if (status == 'OK') {
+      const verifyResult = await this.PaymentService.verifyPayment(
+        authority,
+        data,
+      );
+
+      if (verifyResult.status == 100 || verifyResult.status == 101) {
+        return { success: true, refId: verifyResult.refId };
+      }
+    }
+
+    return { success: false };
   }
 }
