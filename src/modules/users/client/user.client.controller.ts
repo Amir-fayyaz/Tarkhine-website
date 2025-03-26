6ccebedec3315dcc42ef239811b217c8bb4806ed
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-26 08:47:20 -0700

    add getMe api into user.client.controller.ts

--- src/modules/users/client/user.client.controller.ts
+++ src/modules/users/client/user.client.controller.ts
@@ -1,6 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { UserGuard } from 'src/modules/auth/guards/User.guard';
+import { UserAppService } from './user.client.service';
+import { User } from 'src/common/decorators/getUser.decorator';
+import { UserEntity } from '../entities/user.entity';
 
-@Controller('api/v1/client/user')
+@Controller('api/v1/client/users')
+@ApiTags('client-user')
+@ApiBearerAuth()
+@UseGuards(UserGuard)
 export class UserAppController {
-  constructor() {}
+  constructor(private readonly UserService: UserAppService) {}
+
+  @Get('getMe')
+  @ApiOperation({ summary: 'For get profile' })
+  async getMe(@User() user: UserEntity) {
+    return await this.UserService.getProfile(user);
+  }
 }
