cb05a1854f0413bcaf2e812bae2c0d7d0e819661
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-28 07:18:45 -0700

    add getCategories api into category.client.controller.ts

--- src/modules/category/client/controllers/category.client.controller.ts
+++ src/modules/category/client/controllers/category.client.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  HttpCode,
+  HttpStatus,
+  ParseIntPipe,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { SkipThrottle } from '@nestjs/throttler';
 import { UserGuard } from 'src/modules/auth/guards/User.guard';
+import { CategoryAppService } from '../services/category.client.service';
 
 @Controller('api/v1/client/category')
 @ApiTags('client-Category')
 @UseGuards(UserGuard)
 @SkipThrottle()
 export class CategoryAppController {
-  constructor() {}
+  constructor(private readonly CategoryService: CategoryAppService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'For recive categories with pagination' })
+  @ApiQuery({
+    name: 'page',
+    default: 1,
+    required: false,
+    description: 'For pagination',
+  })
+  @HttpCode(HttpStatus.OK)
+  async getCategories(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    return await this.CategoryService.getCategories(page);
+  }
 }
