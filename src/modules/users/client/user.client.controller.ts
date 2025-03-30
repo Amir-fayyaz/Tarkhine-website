746e3ea59b6b894de02f28bb9dd49b25307cd5e2
Amir-Fayyaz <fayamir54@gmail.com>
2025-03-30 09:16:47 -0700

    add editProfile api into user.client.controller.ts

--- src/modules/users/client/user.client.controller.ts
+++ src/modules/users/client/user.client.controller.ts
@@ -1,10 +1,25 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserGuard } from 'src/modules/auth/guards/User.guard';
 import { UserAppService } from './user.client.service';
 import { User } from 'src/common/decorators/getUser.decorator';
 import { UserEntity } from '../entities/user.entity';
 import { SkipThrottle } from '@nestjs/throttler';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('api/v1/client/users')
 @ApiTags('client-user')
@@ -18,5 +33,16 @@ export class UserAppController {
   @ApiOperation({ summary: 'For get profile' })
   async getMe(@User() user: UserEntity) {
     return await this.UserService.getProfile(user);
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: 'For update profile' })
+  @ApiParam({ name: 'id', description: 'user-id' })
+  @ApiBody({ type: UpdateUserDto, description: 'required fields' })
+  async editProfile(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() data: UpdateUserDto,
+  ) {
+    return await this.UserService.editProfile(id, data);
   }
 }
