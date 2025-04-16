17066d39e0c9bd04c5cb542b4929bebb9c85af1c
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-16 13:14:09 -0700

    add getTotalOrders api into order.admin.controller.ts

--- src/modules/orders/admin/controllers/order.admin.controller.ts
+++ src/modules/orders/admin/controllers/order.admin.controller.ts
@@ -1,13 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { OrderAdminService } from '../services/order.admin.service';
 import { SkipThrottle } from '@nestjs/throttler';
-import { ApiBearerAuth, ApiHeader, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiHeader,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AdminGuard } from 'src/modules/auth/guards/Admin.guard';
 
 @Controller('api/v1/admin/orders')
 @ApiTags('admin-order')
 @ApiBearerAuth()
 @ApiHeader({ name: 'authorization' })
+@UseGuards(AdminGuard)
 @SkipThrottle()
 export class OrderAdminController {
   constructor(private readonly OrderService: OrderAdminService) {}
+
+  @Get('count')
+  @ApiOperation({ summary: 'For get total orders' })
+  async getTotalOrders() {
+    return await this.OrderService.getTotalOrders();
+  }
 }
