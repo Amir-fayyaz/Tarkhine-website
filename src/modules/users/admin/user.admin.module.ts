517ae35fe43c78578852d8d4241d04b1edf13111
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-16 12:05:25 -0700

    add adminGuard dependency

--- src/modules/users/admin/user.admin.module.ts
+++ src/modules/users/admin/user.admin.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../entities/user.entity';
+import { UserAdminController } from './user.admin.controller';
+import { UserAdminService } from './user.admin.service';
+import { JwtService } from '@nestjs/jwt';
+import { AuthAdminFactory } from 'src/modules/auth/admin/auth.admin.factory';
+import { AuthAdminService } from 'src/modules/auth/admin/auth.admin.service';
+import { AdminEntity } from 'src/modules/auth/entities/admin.entity';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([UserEntity, AdminEntity])],
+  controllers: [UserAdminController],
+  providers: [UserAdminService, JwtService, AuthAdminFactory, AuthAdminService],
 })
 export class UserAdminModule {}
