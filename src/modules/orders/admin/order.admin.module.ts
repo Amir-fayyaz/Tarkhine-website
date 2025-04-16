49c5178b91f8bf80da6f1910d38334d14ea434b2
Amir-Fayyaz <fayamir54@gmail.com>
2025-04-16 13:13:05 -0700

    add AdminGuard dependencies

--- src/modules/orders/admin/order.admin.module.ts
+++ src/modules/orders/admin/order.admin.module.ts
@@ -3,10 +3,19 @@ import { OrderAdminController } from './controllers/order.admin.controller';
 import { OrderAdminService } from './services/order.admin.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderEntity } from '../entities/order.entity';
+import { AdminEntity } from 'src/modules/auth/entities/admin.entity';
+import { JwtService } from '@nestjs/jwt';
+import { AuthAdminService } from 'src/modules/auth/admin/auth.admin.service';
+import { AuthAdminFactory } from 'src/modules/auth/admin/auth.admin.factory';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderEntity])],
+  imports: [TypeOrmModule.forFeature([OrderEntity, AdminEntity])],
   controllers: [OrderAdminController],
-  providers: [OrderAdminService],
+  providers: [
+    OrderAdminService,
+    JwtService,
+    AuthAdminService,
+    AuthAdminFactory,
+  ],
 })
 export class OrderAdminModule {}
